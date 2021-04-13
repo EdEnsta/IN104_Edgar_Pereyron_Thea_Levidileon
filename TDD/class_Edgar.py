53756fcac69857f2f2655b723e1789b7b94e90ff
Edgar Pereyron <edgar.pereyron@ensta-paris.fr>
2021-04-13 17:44:40 +0200

    Classes Edgar modifiées

--- TDD/class_Edgar.py
+++ TDD/class_Edgar.py
@@ -1,7 +1,26 @@
 class vehicle:
 
-    def __init__(self,horsepower,color,model):
+    def __init__(self,horsepower,color,brand):
         self.horsepower = horsepower
         self.color = color
-        self.model = model
+        self.brand = brand
 
+class truck (vehicle):
+    def attributes(self,nb_wheels,weight):
+        self.nb_wheels = nb_wheels
+        self.weight = weight
+
+    def get_weight(self):
+        return("This truck weighs " +str(self.weight)+ "tons. ")
+
+
+class car (vehicle):
+    def attributes(self,nb_of_places,type):
+        self.nb_of_places = nb_of_places
+        self.type = type  # voiture de sport, voiture utilitaire etc...
+
+    def test_type(self, ref):
+        if (self.type == ref):
+            return ("The car has a matched type.")
+        else:
+            return("The type does not match.")
